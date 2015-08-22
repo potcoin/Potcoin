f2dcc7f577ba8603220c13ba5dc9d8ec475dc6ae
cryptovein <cryptovein@gmail.com>
2015-08-22 11:42:52 -0400

    Update main.h

--- src/main.h
+++ src/main.h
@@ -1415,7 +1415,7 @@ class CBlockHeader
 {
 public:
     // header
-    static const int CURRENT_VERSION=3;
+    static const int CURRENT_VERSION=1;
     int nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
